9b1a0db40294397d7e0b5ec3812b9686565229ac
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-10-18 12:12:28 +0530

    Added Lombok

--- src/main/java/com/harish/recipe/services/RecipeServiceImpl.java
+++ src/main/java/com/harish/recipe/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.harish.recipe.services;
 
 import com.harish.recipe.domain.Recipe;
 import com.harish.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service!");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
