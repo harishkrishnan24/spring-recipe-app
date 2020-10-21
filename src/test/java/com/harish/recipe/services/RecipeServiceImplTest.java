2bf7e09329e2565d2812f666eae39b4fb438964d
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-10-21 09:10:17 +0530

    UOM integration test

---
+++ src/test/java/com/harish/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.harish.recipe.services;
+
+import com.harish.recipe.domain.Recipe;
+import com.harish.recipe.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
