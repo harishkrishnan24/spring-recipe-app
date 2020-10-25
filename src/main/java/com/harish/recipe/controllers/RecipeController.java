e9cc6382ebb65570836b7de7b6525532017d2158
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-10-25 17:09:50 +0530

    Delete Recipe

--- src/main/java/com/harish/recipe/controllers/RecipeController.java
+++ src/main/java/com/harish/recipe/controllers/RecipeController.java
@@ -2,14 +2,13 @@ package com.harish.recipe.controllers;
 
 import com.harish.recipe.commands.RecipeCommand;
 import com.harish.recipe.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Slf4j
 public class RecipeController {
 
     private final RecipeService recipeService;
@@ -44,5 +43,15 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+
+        return "redirect:/";
     }
 }
